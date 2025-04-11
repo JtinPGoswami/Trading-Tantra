a18b099458978a7344f148d66cc19ff479cae74f
Sandeep Patidar <patidarsandeep309@gmail.com>
2025-04-11 21:23:35 +0530

    add percentage cgange to response

--- Backend/src/models/Contraction.model.js
+++ Backend/src/models/Contraction.model.js
@@ -5,6 +5,7 @@ const ContractionSchema = new mongoose.Schema(
     securityId: { type: String, required: true, unique: true },
     UNDERLYING_SYMBOL: { type: String, required: true },
     SYMBOL_NAME: { type: String, required: true },
+    percentageChange: { type: String },
     timestamp: {
       type: Date,
       required: true,
